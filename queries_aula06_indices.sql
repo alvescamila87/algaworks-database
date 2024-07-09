037968af468efe5c1c857ec83333bd3cf8ef8bb6
Camila <alvescamila87@gmail.com>
2024-07-09 19:16:47 -0300

    index column

--- queries_aula06_indices.sql
+++ queries_aula06_indices.sql
@@ -1,1 +1,14 @@
-/* trabalhando com índices */
+/* trabalhando com índices: favorece performance do banco para a pesquisa ficar mais rápida (tipo a PK) */
+
+select * from cliente;
+
+/* verificar c columa TYPE */
+explain select * from cliente;
+
+explain select * from cliente 
+where nome = 'Princesa Isabel';
+
+/* adicionar índice ao nome: ele não cria uma tabela, ele apenas indexa o campo escolhido */
+/* colocou ref em type quando executa o explain */
+alter table cliente
+add index ix_nome (nome);
